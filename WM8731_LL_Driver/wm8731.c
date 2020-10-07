5b11de8eb880f583bf8c83afe357f6ed1e420395
Morgan G <58739911+MorganG69@users.noreply.github.com>
2020-10-07 21:04:49 +0100

    update

--- WM8731_LL_Driver/wm8731.c
+++ WM8731_LL_Driver/wm8731.c
@@ -1,29 +1,102 @@
-// Simple codec setup.
-void wm8731_init(void)
-{
-	 wm8731_ctl_transmit(RESET_CTL, RESET_DEVICE);
-	 wm8731_ctl_transmit(POWER_CTL, ALL_PERIPH_OFF);
-	 wm8731_ctl_transmit(AAP_CTL, I2S_MODE);
-	 LL_mDelay(1);
-	 wm8731_ctl_transmit(DAIFMT, MASTER_16BIT_MSBL);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include "wm8731.h"
 
-}
+wm8731_handle wm8731;
 
 // Enable line input to output bypass.
-void wm8731_bypass_enable(void)
+void wm8731_bypass_enable(wm8731_handle w)
 {
-	 wm8731_ctl_transmit(AAP_CTL, BYPASS_ENABLE);
-	 wm8731_ctl_transmit(POWER_CTL, OUTPUTS_ENABLE);
-	 wm8731_ctl_transmit(POWER_CTL, LINE_INPUTS_ENABLE);
+	wm8731_ctl_set(w, 3, WM8731_AAP_CTL, WM8731_DAC_SELECT, WM8731_BYPASS_ENABLE, WM8731_MIC_MUTE);
+	wm8731_ctl_transmit(w->i2cHandle, WM8731_AAP_CTL, w->analogPath);
+
+	wm8731_ctl_transmit(w->i2cHandle, WM8731_POWER_CTL, ALL_PERIPH_OFF);
+
+	wm8731_ctl_set(w, 3, WM8731_POWER_CTL, WM8731_OUTPUTS_ENABLE, WM8731_LINEINPUT_ENABLE, WM8731_POWER_ON);
+	wm8731_ctl_transmit(w->i2cHandle, WM8731_POWER_CTL, w->power);
 }
 
-// Transmit control data to the codec over I2C
-void wm8731_ctl_transmit(uint8_t reg, uint8_t data)
+wm8731_handle *wm8731_init(I2C_TypeDef I2Cx)
+{
+	wm8731_handle *temp;
+
+	temp->i2cHandle = I2Cx;
+
+	wm8731_ctl_transmit(I2Cx, WM8731_RESET_CTL, WM8731_RESET_DEVICE);
+	wm8731_ctl_transmit(I2Cx, WM8731_POWER_CTL, WM8731_ALL_PERIPH_OFF);
+
+	temp->lineIn = 0;
+	temp->lineOut = 0;
+	temp->analogPath = 0;
+	temp->digitalPath = 0;
+	temp->power = WM8731_ALL_PERIPH_OFF;
+	temp->digitalFmt = 0;
+	temp->sampleRate = 0;
+
+
+	//wm8731_ctl_transmit(I2Cx, WM8731_AAP_CTL, WM8731_I2S_MODE);
+	//wm8731_ctl_transmit(I2Cx, WM8731_DAIFMT, WM8731_MASTER_16BIT_MSBL);
+
+	return temp;
+}
+
+/* 
+* Edits the codec handle with updated register values.
+* The control transmit function uses the values inside the handle.
+* ARGS: 
+* Codec handle, number of args, register to edit, args to change (...).
+*/
+void wm8731_ctl_set(wm8731_handle w, uint8_t argNum, uint8_t reg, ...)
+{
+	va_list argList;
+	uint8_t count = 0;
+	uint8_t output;
+
+	va_start(argList, reg);
+
+	switch(reg)
+	{
+		// Right line in and out use same values as left.
+		case WM8731_LLINEIN_CTL:
+			output = w->lineIn;
+			break;
+		case WM8731_LHPOUT_CTL:
+			output = w->lineOut;
+			break;
+		case WM8731_AAP_CTL:
+			output = w->analogPath;
+			break;
+		case WM8731_DAP_CTL:
+			output = w->digitalPath;
+			break;
+		case WM8731_POWER_CTL:
+			output = w->power;
+			break;
+		case WM8731_DAIFMT:
+			output = w->digitalFmt;
+			break;
+		case WM8731_SAMPLE_CTL:
+			output = w->sampleRate;
+			break;
+	}
+
+	for(count = 0 ; count < argNum ; count++)
+	{
+		SET_BIT(output, va_arg(argList, int));
+	}
+
+	va_end(argList);
+}
+
+// Transmit control data.
+void wm8731_ctl_transmit(I2C_TypeDef I2Cx, uint8_t reg, uint8_t data)
 {
 	uint8_t transmitBuf[2];
 
 	transmitBuf[0] = (reg << 1); // Shift reg address by one
 	transmitBuf[1] = data;
 
-	i2cTransmit(I2C1, WM8731_ADDR, transmitBuf, 2);
+	i2cTransmit(I2Cx, WM8731_ADDR, transmitBuf, 2);
 }
--- WM8731_LL_Driver/wm8731.h
+++ WM8731_LL_Driver/wm8731.h
@@ -1,36 +1,137 @@
 #define WM8731_ADDR 0x34 // Actual address is 0x1A but has to be left shifted for the R/W bit.
 
-#define I2S_MODE 0x02
+// Inputs and outputs
+//#define WM8731_BYPASS_ENABLE 0x1A // Enables the DAC and bypass, mutes the ADC mic input.
+//#define WM8731_OUTPUTS_ENABLE 0x6F
+//#define WM8731_LINE_INPUTS_ENABLE 0x6E
+//#define WM8731_LINE_INPUTS_MUTE 0x80
 
-#define DATA_LENGTH_16 0x00
-#define DATA_LENGTH_20 0x01
-#define DATA_LENGTH_24 0x02
-#define DATA_LENGTH_32 0x03
+// Line input control
+#define WM8731_LINEINPUT_MUTE 0x80
+#define WM8731_LINEINPUT_UNMUTE 0x00
 
-#define BYPASS_ENABLE 0x1A // Enables the DAC and bypass, mutes the ADC mic input.
-#define OUTPUTS_ENABLE 0x6F
-#define LINE_INPUTS_ENABLE 0x6E
+// Headphone output control
+#define WM8731_HP_ZEROCROSS_ENABLE 0x80
+#define WM8731_HP_ZEROCROSS_DISABLE 0x00
 
-#define STANDBY 0x7F
-#define POWER_OFF 0xE0
-#define RESET_DEVICE 0x00
-#define ALL_PERIPH_OFF 0x7F
+// Analogue audio path control
+#define WM8731_MIC_BOOST_ENABLE 0x01
+#define WM8731_MIC_BOOST_DISABLE 0x00
+#define WM8731_MIC_MUTE 0x02
+#define WM8731_MIC_UNMUTE 0x00
+#define WM8731_ADC_INPUT_MIC 0x04
+#define WM8731_ADC_INPUT_LINE 0x00
+#define WM8731_BYPASS_ENABLE 0x08
+#define WM8731_BYPASS_DISABLE 0x00
+#define WM8731_DAC_SELECT 0x10
+#define WM8731_DAC_DESELECT 0x00
+#define WM8731_SIDETONE_15DB 0xC0
+#define WM8731_SIDETONE_12DB 0x80
+#define WM8731_SIDETONE_9DB 0x40
+#define WM8731_SIDETONE_6DB 0x00
 
-#define MASTER_16BIT_MSBL 0x42 // MSBL m.s.b first left justified
+// Digital audio path control
+#define WM8731_ADC_HPF_ENABLE 0x01
+#define WM8731_ADC_HPF_DISABLE 0x00
+#define WM8731_DEEMP_48K 0x06
+#define WM8731_DEEMP_44K 0x04
+#define WM8731_DEEMP_32K 0x02
+#define WM8731_DEEMP_DISABLE 0x00
+#define WM8731_DAC_MUTE 0x80
+#define WM8731_DAC_UNMUTE 0x00
+#define WM8731_HPF_DCSTORE 0x10
+#define WM8731_HPF_DCCLEAR 0x00
+
+// Power down control
+#define WM8731_ALL_PERIPH_OFF 0x7F
+#define WM8731_ALL_PERIPH_ON 0x00
+#define WM8731_LINEINPUT_DISABLE 0x01
+#define WM8731_LINEINPUT_ENABLE 0x00
+#define WM8731_MICINPUT_DISABLE 0x02
+#define WM8731_MICINPUT_ENABLE 0x00
+#define WM8731_ADC_DISABLE 0x04
+#define WM8731_ADC_ENABLE 0x00
+#define WM8731_DAC_DISABLE 0x08
+#define WM8731_DAC_ENABLE 0x00
+#define WM8731_OUTPUTS_DISABLE 0x10
+#define WM8731_OUTPUTS_ENABLE 0x00
+#define WM8731_OSC_DISABLE 0x20
+#define WM8731_OSC_ENABLE 0x00
+#define WM8731_CLKOUT_DISABLE 0x40
+#define WM8731_CLKOUT_ENABLE 0x00
+#define WM8731_POWER_OFF 0x80
+#define WM8731_POWER_ON 0x00
+
+// Digital audio interface format
+#define WM8731_FMT_DSP 0x03
+#define WM8731_FMT_I2S 0x02
+#define WM8731_FMT_MSBL 0x01
+#define WM8731_FMT_MSBR 0x00
+#define WM8731_FMT_32BIT 0x0C
+#define WM8731_FMT_24BIT 0x08
+#define WM8731_FMT_20BIT 0x04
+#define WM8731_FMT_16BIT 0x00
+#define WM8731_DACLRC_PHASE_HIGH 0x10
+#define WM8731_DACLRC_PHASE_LOW 0x00
+#define WM8731_DAC_CLKSWAP_ENABLE 0x20
+#define WM8731_DAC_CLKSWAP_DISABLE 0x00
+#define WM8731_MASTER_MODE 0x40
+#define WM8731_SLAVE_MODE 0x00
+#define WM8731_BITCLK_INVERT 0x80
+#define WM8731_BITCLK_NONINVERT 0x00
+
+// Sampling control
+#define WM8731_USB_MODE 0x01
+#define WM8731_NORMAL_MODE 0x00
+#define WM8731_BOSR_256FS 0x00
+#define WM8731_BOSR_384FS 0x02
+#define WM8731_CORECLK_DIV2 0x40
+#define WM8731_CORECLK_NODIV 0x00
+#define WM8731_CLKOUT_DIV2 0x80
+#define WM8731_CLKOUT_NODIV 0x00
+
+// Active control
+#define WM8731_ACTIVE_ENABLE 0x01
+#define WM8731_ACTIVE_DISABLE 0x00
+
+// Reset control
+#define WM8731_RESET 0x00
 
 // Register addresses.
-#define LLINEIN_CTL 0x00
-#define RLINEIN_CTL 0x01
-#define LHPOUT_CTL 0x02
-#define RHPOUT_CTL 0x03
-#define AAP_CTL 0x04
-#define DAP_CTL 0x05
-#define POWER_CTL 0x06
-#define DAIFMT 0x07
-#define SAMPLE_CTL 0x08
-#define ACTIVE_CTL 0x09
-#define RESET_CTL 0x0F
+#define WM8731_LLINEIN_CTL 0x00
+//#define WM8731_RLINEIN_CTL 0x01
+#define WM8731_LHPOUT_CTL 0x02
+//#define WM8731_RHPOUT_CTL 0x03
+#define WM8731_AAP_CTL 0x04
+#define WM8731_DAP_CTL 0x05
+#define WM8731_POWER_CTL 0x06
+#define WM8731_DAIFMT 0x07
+#define WM8731_SAMPLE_CTL 0x08
+#define WM8731_ACTIVE_CTL 0x09
+#define WM8731_RESET_CTL 0x0F
 
+// Device handle
+typedef struct 
+{
+	// Handle to the I2C peripheral controlling the codec.
+	I2C_TypeDef i2cHandle;
+	/*
+	* These hold the current register values. 
+	* So individual bits can be changed without ORing the whole thing together everytime.	
+	*/
+	uint8_t lineIn;
+	uint8_t lineOut;
+	uint8_t analogPath;
+	uint8_t digitalPath;
+	uint8_t power;
+	uint8_t digitalFmt;
+	uint8_t sampleRate;
+}wm8731_handle_t;
+
+typedef wm8731_handle_t *wm8731_handle;
+
+// Control functions
 void wm8731_ctl_transmit(uint8_t reg, uint8_t data);
-void wm8731_init(void);
-void wm8731_bypass_enable(void);
+wm8731_handle *wm8731_init(void);
+void wm8731_bypass_enable(wm8731_handle w);
+
